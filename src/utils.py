570ca24d2570bbec6ec49099a45a501523337427
itrxiAi <itrix.ai.global@gmail.com>
2025-07-18 19:53:25 +0800

    price fix

--- src/utils.py
+++ src/utils.py
@@ -302,8 +302,8 @@ def calculate_standard_deviation(price_changes):
 def calculate_target_price(btc_price):
     # Define the ranges
     btc_min = 0
-    btc_max = 230000
-    btc_mid = 115000
+    btc_max = 234000
+    btc_mid = 117000
     
     # Define target price bounds
     target_min = 0
