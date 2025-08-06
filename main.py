6a96689fbcde23d8db7b174d77766ed9bd5209f8
itrxiAi <itrix.ai.global@gmail.com>
2025-08-06 15:20:51 +0800

    order change

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.market_making import market_making
 if __name__ == "__main__":
     try:
 
-        market_making(200, 100, 300, 100, 80, 0.00009)
+        market_making(50, 10, 10, 100, 20, 0.00009)
 
     except KeyboardInterrupt:
         print("Main process interrupted")
